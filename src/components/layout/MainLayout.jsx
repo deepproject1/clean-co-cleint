2bd558b5e1c421d6868c23a3a61b61e38176b040
Dibyendu Pramanik <chotuu668@gmil.com>
2024-01-03 11:07:19 +0530

    update

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,41 +1,44 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const MainLayout = () => {
+const MainLayout = ({ children }) => {
     return (
-    <>
-    <div className="drawer">
-  <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
-  <div className="drawer-content flex flex-col">
-    {/* Navbar */}
-    <div className="w-full navbar bg-base-300">
-      <div className="flex-none lg:hidden">
-        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        </label>
-      </div> 
-      <div className="flex-1 px-2 mx-2">Navbar Title</div>
-      <div className="flex-none hidden lg:block">
-        <ul className="menu menu-horizontal">
-          {/* Navbar menu content here */}
-          <li><a>Navbar Item 1</a></li>
-          <li><a>Navbar Item 2</a></li>
-        </ul>
-      </div>
-    </div>
-    {/* Page content here */}
-    Content
-  </div> 
-  <div className="drawer-side">
-    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-80 min-h-full bg-base-200">
-      {/* Sidebar content here */}
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-  </div>
-</div>
-    </>
+        <>
+            <div className="drawer">
+                <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col">
+                    {/* Navbar */}
+                    <div className="w-full navbar bg-base-300">
+                        <div className="flex-none lg:hidden">
+                            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            </label>
+                        </div>
+                        <div className="flex-1 px-2 mx-2">Navbar Title</div>
+                        <div className="flex-none hidden lg:block">
+                            <div className="flex gap-2">
+                                {/* Navbar menu content here */}
+                                <NavLink to='/about' className={({ isActive }) => isActive ? "btn btn-primary btn-sm " : "btn btn-ghost btn-sm"} >About</NavLink>
+                                <NavLink to='/contact' className={({ isActive }) => isActive ? "btn btn-primary btn-sm " : "btn btn-ghost btn-sm"}>Contact</NavLink>
+                                <NavLink to='/login' className={({ isActive }) => isActive ? "btn btn-primary btn-sm " : "btn btn-ghost btn-sm"}>Login</NavLink>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Page content here */}
+                    {children}
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <div className="menu p-4 w-80 min-h-full bg-base-200">
+                        {/* Sidebar content here */}
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,48 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import AdminLayout from "../components/layout/AdminLayout";
+import AddService from "../Pages/AddService";
 
 
 const routes = createBrowserRouter([
     {
     path:'/',
-       element:<App/>
-    }
+    element:<App/>,
+    children:[
+        {
+            path:'about',
+            element:<About/>
+        },
+        {
+            path:'contact',
+            element:<Contact/>
+        },
+    ]
+    },
+    {
+        path:'/login',
+        element:<Login/>
+    },
+    {
+        path:'/register',
+        element:<Register/>
+    },
+    {
+        path:'/admin',
+        element:<AdminLayout/>,
+        children:[
+            {
+                path:'addService',
+                element:<AddService/>
+            }
+        ]
+    },
+
+
 ])
 
 
